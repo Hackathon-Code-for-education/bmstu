be08eaed4e1e5db1d2556720b6fe04a974905fd5
Ark <A6611688@yandex.ru>
2024-04-20 14:24:40 +0300

    asd

--- plswork/moderation.py
+++ plswork/moderation.py
@@ -3,4 +3,4 @@ from transformers import pipeline
 pipe = pipeline("text2text-generation", model="abhiai/ModerationGPT")
 
 print(pipe("Hello world!")[0]['generated_text'])
-print(pipe("fuck you")[0]['generated_text'])
+print(pipe("fuck youч")[0]['generated_text'])
