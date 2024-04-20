a4c5ee598e1747e3e4d8349f3d1b3f7af9b14a0d
Ark <A6611688@yandex.ru>
2024-04-20 19:56:52 +0300

    nothing

--- plswork/text_recognition.py
+++ plswork/text_recognition.py
@@ -1,10 +1,11 @@
 import pytesseract
 import cv2
+# import transformers
 # import matplotlib.pyplot as plt
 # from PIL import Image
 
-image = cv2.imread("stud1.jpg")
+image = cv2.imread("stud3.png")
 # print(image)
-pytesseract.pytesseract.tesseract_cmd = r".\.venv\Lib\site-packages\pytesseract"
-string = pytesseract.image_to_string(image)
+pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
+string = pytesseract.image_to_string(image,  lang='rus')
 print(string)
