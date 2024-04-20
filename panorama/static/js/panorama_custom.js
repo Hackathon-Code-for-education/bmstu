cf21999c69b9185f01e0294d318977dee77de23a
HumsterProgrammer <denis_ka_2307@mail.ru>
2024-04-20 19:52:58 +0300

    double panorama created

--- panorama/static/js/panorama_custom.js
+++ panorama/static/js/panorama_custom.js
@@ -4,71 +4,249 @@ ymaps.ready(function () {
         // Если нет, то ничего не будем делать.
         return;
     }
+    // Создаем объект, содержащий данные трех связанных панорам.
+    // Первые две панорамы мы создали сами, а третья панорама - это Яндекс.Панорама.
+    // Переход между первыми двумя панорамами зададим через стандартные стрелки,
+    // а переход с первой панорамы на Яндекс.Панораму - через маркер-переход.
+    var panoData = {
+        // Данные первой панорамы.
+        firstPano: {
+            type: 'custom',
+            angularBBox: [Math.PI / 2, 2 * Math.PI + Math.PI / 4, -Math.PI / 2, Math.PI / 4],
+            position: [0, 0, 0],
+            tileSize: [512, 512],
+            tileLevels: [{
+                getTileUrl: function (x, y) {
+                    return 'uploads/tiles/1/route2/0/lq/' + x + '-' + y + '.jpg';
+                },
+                getImageSize: function () {
+                    return [3584, 1792];
+                }
+            }/*, {
+                getTileUrl: function (x, y) {
+                    return 'tiles/panorama1/lq/' + x + '-' + y + '.jpg';
+                },
+                getImageSize: function () {
+                    return [512, 256];
+                }
+            }*/],
+            // Переходы на панораме по стандартной стрелке.
+            connectionArrows: [{
+                panoID: 'secondPano',
+                direction: [-45, 0]
+            }],
+            // Переходы на панораме через маркеры.
+            markerConnections: [/*{
+                panoID: 'thirdPano',
+                iconSrc: {
+                    'default': 'images/marker-default.jpg',
+                    hovered: 'images/marker-hovered.jpg'
+                },
+                iconPosition: [0.5, 1.5, 0.3]
+            }*/]
+        },
+        // Данные второй панорамы.
+        secondPano: {
+            type: 'custom',
+            angularBBox: [Math.PI / 2, 2 * Math.PI, -Math.PI / 2, 0],
+            position: [0, 0, 0],
+            tileSize: [512, 512],
+            tileLevels: [{
+                getTileUrl: function (x, y) {
+                    return 'uploads/tiles/1/route2/1/lq/' + x + '-' + y + '.jpg';
+                },
+                getImageSize: function () {
+                    return [3584, 1792];
+                }
+            }, {
+                getTileUrl: function (x, y) {
+                    return 'tiles/panorama2/lq/' + x + '-' + y + '.jpg';
+                },
+                getImageSize: function () {
+                    return [512, 256];
+                }
+            }],
+            // Добавим обратный переход на первую панораму.
+            connectionArrows: [{
+                panoID: 'firstPano',
+                direction: [90, 0]
+            }],
+            markerConnections: []
+        },
+        // Яндекс.Панорама.
+        // Информацию о панораме будем получать далее с серверов Яндекса
+        // с помощью функции ymaps.panorama.locate.
+    };
 
-    // Сначала описываем уровни масштабирования панорамного изображения.
-    // Для этого заводим класс, реализующий интерфейс IPanoramaTileLevel.
-    // Параметрами конструктора будут шаблон URL тайлов и размер уровня.
-    function TileLevel (urlTemplate, imageSize) {
-        this._urlTemplate = urlTemplate;
-        this._imageSize = imageSize;
+    // Функция для извлечения данных нужной панорамы из объекта panoData.
+    function getConnectedPanoramaData(panoID) {
+        return panoData[panoID];
+    }
+    // Функция, загружающая изображение маркера с сервера.
+    function loadImage(src) {
+        return new ymaps.vow.Promise(function (resolve) {
+            var image = new Image();
+            image.onload = function () {
+                resolve(image);
+            };
+            image.crossOrigin = 'anonymous';
+            image.src = src;
+        });
     }
 
-    ymaps.util.defineClass(TileLevel, {
-        getTileUrl: function (x, y) {
-            // Определяем URL тайла для переданных индексов.
-            return this._urlTemplate.replace('%c', x + '-' + y);
-        },
+    // Создаем класс, описывающий переход между панорамами по стандартной стрелке.
+    function ConnectionArrow(currentPanorama, direction, nextPanorama) {
+        this.properties = new ymaps.data.Manager();
+        this._currentPanorama = currentPanorama;
+        this._direction = direction;
+        this._connectedPanorama = nextPanorama;
+    }
 
-        getImageSize: function () {
-            return this._imageSize;
+    ymaps.util.defineClass(ConnectionArrow, {
+        getConnectedPanorama: function () {
+            // Если переход будет осуществляться на пользовательскую панораму,
+            // то создаем объект панорамы MyPanorama.
+            // Если нужно перейти на Яндекс.Панораму, то для получения объекта
+            // панорамы воспользуемся функцией ymaps.panorama.locate.
+            if (this._connectedPanorama.type == 'custom') {
+                return ymaps.vow.resolve(new MyPanorama(this._connectedPanorama));
+            } else if (this._connectedPanorama.type == 'yandex') {
+                return ymaps.panorama.locate(this._connectedPanorama.coords).then(
+                    function(panoramas) {
+                        if (panoramas.length) {
+                            return panoramas[0];
+                        }  else {
+                            return ymaps.vow.reject(new Error('Панорама не нашлась.'));
+                        }
+                    }
+                );
+            }
+        },
+        // Направление взгляда на панораму, на которую будет осуществляться переход.
+        getDirection: function () {
+            return this._direction;
+        },
+        // Ссылка на текущую панораму, из которой осуществляется переход.
+        getPanorama: function () {
+            return this._currentPanorama;
+        }
+    })
+
+    // Создаем класс, описывающий маркер-переход.
+    function MarkerConnection(currentPanorama, imgSrc, position, nextPanorama) {
+        // В классе должно быть определено поле properties.
+        this.properties = new ymaps.data.Manager();
+        this._panorama = currentPanorama;
+        this._position = position;
+        this._imgSrc = imgSrc;
+        this._connectedPanorama = nextPanorama;
+    }
+
+    ymaps.util.defineClass(MarkerConnection, {
+        getIconSet: function () {
+            return ymaps.vow.Promise.all([
+                loadImage(this._imgSrc.default),
+                loadImage(this._imgSrc.hovered)
+            ]).spread(function (defaultImage, hoveredImage) {
+                return {
+                    'default': {
+                        image: defaultImage,
+                        offset: [0, 0]
+                    },
+                    hovered: {
+                        image: hoveredImage,
+                        offset: [0, 0]
+                    }
+                };
+            });
+        },
+        // Текущая панорама, из которой осуществляется переход.
+        getPanorama: function () {
+            return this._panorama;
+        },
+        // Позиция маркера на текущей панораме.
+        getPosition: function () {
+            return this._position;
+        },
+        // Чтобы по клику на маркер осуществлялся переход на другую панораму,
+        // реализуем метод getConnectedPanorama.
+        getConnectedPanorama: function () {
+            if (this._connectedPanorama.type == 'custom') {
+                return ymaps.vow.resolve(new MyPanorama(this._connectedPanorama));
+            } else if (this._connectedPanorama.type == 'yandex') {
+                return ymaps.panorama.locate(this._connectedPanorama.coords).then(
+                    function(panoramas) {
+                        if (panoramas.length) {
+                            return panoramas[0];
+                        } else {
+                            return ymaps.vow.reject(new Error('Панорама не нашлась.'));
+                        }
+                    }
+                );
+            }
         }
     });
 
-    // Теперь описываем панораму.
-    function Panorama () {
+    // Класс панорамы.
+    function MyPanorama(obj) {
         ymaps.panorama.Base.call(this);
-        // Наша панорама будет содержать два уровня масштабирования
-        // панорамного изображения: низкого и высокого качества.
-        this._tileLevels = [
-            //new TileLevel('uploads/tiles/lq/%c.jpg', [512, 256]),
-            new TileLevel('uploads/tiles/1/route2/0/lq/%c.jpg', [3584, 1792])
-        ];
+        this._angularBBox = obj.angularBBox;
+        this._position = obj.position;
+        this._tileSize = obj.tileSize;
+        this._tileLevels = obj.tileLevels;
+        // Получаем массив экземпляров класса, описывающего переход по стрелке из
+        // одной панорамы на другую.
+        this._connectionArrows = obj.connectionArrows.map(function (connectionArrow) {
+            return new ConnectionArrow(
+                this, // Текущая панорама.
+                connectionArrow.direction, // Направление взгляда на панораму, на которую делаем переход.
+                getConnectedPanoramaData(connectionArrow.panoID) // Данные панорамы, на которую делаем переход.
+            );
+        }, this);
+        // Получаем массив маркеров-переходов.
+        this._connectionMarkers = obj.markerConnections.map(function (marker) {
+            return new MarkerConnection(
+                this, // Текущая панорама.
+                marker.iconSrc, // Изображение маркера.
+                marker.iconPosition, // Позиция маркера.
+                getConnectedPanoramaData(marker.panoID) // Данные панорамы, на которую делаем переход.
+            );
+        }, this);
+
     }
 
-    // Наследуем класс панорамы от ymaps.panorama.Base, который частично
-    // реализует IPanoramaTileLevel за нас.
-    ymaps.util.defineClass(Panorama, ymaps.panorama.Base, {
-        getPosition: function () {
-            // Панорама будет располагаться в начале координат...
-            return [0, 0, 0];
+    ymaps.util.defineClass(MyPanorama, ymaps.panorama.Base, {
+        // Чтобы добавить на панораму стандартные стрелки переходов,
+        // реализуем метод getConnectionArrows.
+        getConnectionArrows: function () {
+            return this._connectionArrows;
         },
-
-        getCoordSystem: function () {
-            // ...декартовой системы.
-            return ymaps.coordSystem.cartesian;
+        // Чтобы добавить на панораму маркеры-переходы,
+        // нужно реализовать метод getConnectionMarkers.
+        getConnectionMarkers: function () {
+            return this._connectionMarkers;
         },
-
         getAngularBBox: function () {
-            // Область, которую занимает панорама на панорамной сфере.
-            // В нашем случае это будет вся сфера.
-            return [
-                0.5 * Math.PI,
-                2 * Math.PI,
-                -0.5 * Math.PI,
-                0
-            ];
+            return this._angularBBox;
         },
-
+        getPosition: function () {
+            return this._position;
+        },
         getTileSize: function () {
-            // Размер тайлов, на которые нарезано изображение.
-            return [512, 512];
+            return this._tileSize;
         },
-
         getTileLevels: function () {
             return this._tileLevels;
+        },
+        getCoordSystem: function () {
+            return ymaps.coordSystem.cartesian;
         }
     });
 
-    // Теперь создаем плеер с экземпляром нашей панорамы.
-    var player = new ymaps.panorama.Player('player', new Panorama());
+    var panorama = new MyPanorama(panoData.firstPano);
+
+    // Отображаем панораму на странице.
+    var player = new ymaps.panorama.Player('player', panorama, {
+        direction: [25, 0]
+    });
 });
